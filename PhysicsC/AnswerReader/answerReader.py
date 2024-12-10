2f1962f511e044e09bddc68b6d9efff759437f4c
Harrison Knight <79892001+H20rj@users.noreply.github.com>
2024-12-10 10:33:28 -0600

    Added answer reader files and bowCalc

--- PhysicsC/AnswerReader/answerReader.py
+++ PhysicsC/AnswerReader/answerReader.py
@@ -1,30 +1,32 @@
-#string = 'AADA BCAC DDBA BCBC CBCB DDAA'.replace(' ','')
+
 from os import system
 def main() -> None:
     string = input("Enter a string of answers, no commas: \n").replace(" ", "")
     iterations = 0
     string_len= range(len(string))
     system("clear")
-    for i in string_len:
+    run = True
+    while run:
         iterations += 1
-        jump_to = input("Enter a number to jump to question (enter for next, q to exit): ")
-
+        jump_to = input("Enter a number to jump to question (enter for next, q to exit)...\n")
+        system("clear")
 
         if jump_to.isdigit():
             jump_to_index = int(jump_to) - 1
             if jump_to_index in string_len:
-                print(f'Answer for {jump_to}: {string[int(jump_to_index)]}')
+                print(f'Answer for question {jump_to}: {string[int(jump_to_index)]}')
                 iterations = jump_to_index + 1
             else:
                 print(f"Invalid question number: {jump_to}")
         elif jump_to == 'q':
-            exit(1)
+            run = False
         else:
             if iterations-1 > max(string_len):
                 print("no more answers")
-                exit(2)
-            print(f"Answer for question {iterations}: {string[iterations-1]}")
-        system("clear")
+                run = False
+            else:
+                print(f"Answer for question {iterations}: {string[iterations-1]}")
+
 
 if __name__ == "__main__":
     main()
